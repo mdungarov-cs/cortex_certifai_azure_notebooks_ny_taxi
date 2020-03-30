2db7ef99b72d9ea4cd16fed4a0985424712a732e
mdungarov <mdungarov@cognitivescale.com>
2020-03-30 16:12:58 +0100

    expand utils.py

--- tutorial_utils.py
+++ tutorial_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 # The result is a dictionary keyed on analysis, containing reports keyed on model id
 # The console app is the recommended way to view these, by saving the results to file
 # (see previous cell), but programmatic analysis of the result here is also possible
@@ -51,3 +51,35 @@ def plot_histogram(ax, model_id, result):
     ax.set_title(f'Model: {model_id}')
     ax.set_xticks(indexes)
     ax.set_xticklabels(feature_names[order], rotation=90)
+
+def plot_fairness_burden(df_rslt,group_categories,group_xlabels):
+    nr_grp=len(group_categories)
+    feature_scores = df_rslt[[f"Group burden {ct}" for ct in group_categories]]
+    feature_lower_bounds = df_rslt[[f"Group burden {ct} lower bound" for ct in group_categories]]
+    feature_upper_bounds = df_rslt[[f"Group burden {ct} upper bound" for ct in group_categories]]
+
+    fig, ax = plt.subplots(figsize=[12,4])
+    ax.set_title('Feature fairness by model', fontsize=20)
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:pink']
+    width = 0.4
+
+    ax.set_xticks(np.arange(nr_grp)+width)
+    ax.set_xticklabels(group_xlabels)
+
+    for idx in range(len(df_rslt)):
+        central_values = list(feature_scores.iloc[idx])
+        lower_bounds = list(feature_lower_bounds.iloc[idx])
+        upper_bounds = list(feature_upper_bounds.iloc[idx])
+        lower_errors = [central_values[i] - lower_bounds[i] for i in range(len(central_values))]
+        upper_errors = [upper_bounds[i] - central_values[i] for i in range(len(central_values))]
+
+        ax.bar([width/2+idx*width+f_idx for f_idx in range(nr_grp)],
+                central_values,
+                [width]*nr_grp,
+                yerr=[lower_errors, upper_errors],
+                color=colors[idx],
+                label=df_rslt.index[idx],
+                capsize=10)
+
+    ax.legend()
+    plt.show()
